53ef601c11bf36f5cec3407b2e613732bdf100dc
jantzz <jantz.ang@gmail.com>
2022-05-29 17:19:02 +0800

    Update main.py
    added more imports and added the run_game

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # github page:https://github.com/Dentosal/python-sc2
 # documentation: https://github.com/Dentosal/python-sc2/wiki
 import sc2 as sc
+from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
 from sc2.constants import COMMANDCENTER, SCV
 
-class Bot(sc.BotAI):
+class BigBoy(sc.BotAI):
     async def on_step(self, iteration):
         await self.distribute_workers()
         await self.build_workers()
@@ -13,3 +14,8 @@ class Bot(sc.BotAI):
         for cc in self.units(COMMANDCENTER).ready.noqueue:
             if self.can_afford(SCV) and self.supply_left > 3:
                 await self.do(cc.train(SCV))
+
+run_game(maps.get("AbyssalReefLE"), [
+    Bot(Race.Terran, BigBoy()), 
+    Computer(Race.Terran, Difficulty.Easy)
+], realtime=True)
