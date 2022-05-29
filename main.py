b88f4bfa8c21bdc9df9b25d4445132f627cc9d94
jantzz <jantz.ang@gmail.com>
2022-05-29 16:56:41 +0800

    Update main.py
    placed in library and made class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #github page:https://github.com/Dentosal/python-sc2
 # documentation: https://github.com/Dentosal/python-sc2/wiki
-print("Hello w0rld")
+import sc2  as sc
 
-print("hello world")
+class Bot(sc.BotAI):
+    
