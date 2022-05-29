e474f2e2c11425557d6a9bdf2a4897c4f249e07d
jantzz <jantz.ang@gmail.com>
2022-05-29 20:41:37 +0800

    Update main.py
    added attack function with army and added scouting function for scvs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # github page for the library being used:https://github.com/Dentosal/python-sc2
 # documentation for sc2 library: https://github.com/Dentosal/python-sc2/wiki
-from tkinter import COMMAND
+import random as rand
 import sc2 as sc
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
@@ -22,12 +22,22 @@ class BigBoy(sc.BotAI):
 
         if self.units(BARRACKS).exists:
             await self.build_vespene()
+
+        await self.attack()
     
+    #scouting with worker
+    async def send_scout(self):
+        worker = self.units(SCV).ready
+        if worker.exists:
+            await worker.attack(self.enemy_start_locations)
+
+    #building workers
     async def build_workers(self):
         for cc in self.units(COMMANDCENTER).ready.noqueue:
             if self.can_afford(SCV) and self.supply_left <= 2:
                 await self.do(cc.train(SCV))
 
+    #building supply depots 
     async def build_supplydepo(self):
         if self.supply_left < 3 and not self.already_pending(SUPPLYDEPOT):
             ccs = self.units(COMMANDCENTER).ready
@@ -35,18 +45,20 @@ class BigBoy(sc.BotAI):
                 if self.can_afford(SUPPLYDEPOT):
                     await self.build(SUPPLYDEPOT, near = ccs.first)
 
+    #building barracks
     async def build_barracks(self):
         ccs = self.units(COMMANDCENTER).ready
         if ccs.exists:
             if not self.units(BARRACKS).exists:
                 if self.can_afford(BARRACKS):
                     await self.build(BARRACKS, near = ccs.first)
-
+    #training marines 
     async def train_marines(self):
         if self.supply_army <= 10:
             for br in self.units(BARRACKS).ready.noqueue:
                 await self.do(br.train(MARINE))
 
+    #building refineries 
     async def build_vespene(self):
         for ccs in self.units(COMMANDCENTER).ready: 
             vesp = self.state.vespene_geyser.closer_than(ccs, 15.0)
@@ -58,6 +70,25 @@ class BigBoy(sc.BotAI):
                     break
                 if self.units(REFINERY).closer_than(1.0, v).exists and self.supply_cap > 15:
                     await self.do(worker.build(REFINERY, v)) 
+
+    #attacking with units 
+    async def attack(self):
+        if self.units(MARINE).amount > 9:
+            if len(self.known_enemy_units) > 0:
+                for u in self.units(MARINE).idle:
+                    await self.do(u.attack(rand.choice(self.known_enemy_units)))
+        elif self.units(MARINE).amount > 14:
+            for u in self.units(MARINE).idle:
+                await self.do(u.attack(self.find_enemy(self.state)))
+
+    #finding enemy units
+    def find_enemy(self, state):
+        if len(self.known_enemy_units > 0):
+            return rand.choice(self.known_enemy_units)
+        elif len(self.known_enemy_structures > 0): 
+            return rand.choice(self.known_enemy_structures)
+        else:
+            return self.enemy_start_locations[0]
 
 
 run_game(maps.get("AbyssalReefLE"), [
