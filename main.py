3e7bbf2c1c8a603c3043ece07be619296d84bf56
jantzz <jantz.ang@gmail.com>
2022-05-29 16:44:18 +0800

    Update main.py
    deleted the useless snippets

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
 #github page:https://github.com/Dentosal/python-sc2
 # documentation: https://github.com/Dentosal/python-sc2/wiki 
-def amogus():
-    return True
-
-print("you are the imposter")
-print("Hello world")
-if amogus:
-    print("Jantz is impustor")
