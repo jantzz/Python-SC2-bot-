c969cf00802bc345d1009b1711f1ff8ce0fe7e67
jantzz <jantz.ang@gmail.com>
2022-05-29 16:37:43 +0800

    firs commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#github page:https://github.com/Dentosal/python-sc2
+# documentation: https://github.com/Dentosal/python-sc2/wiki 
+def amogus():
+    return True
+
+print("you are the imposter")
+print("Hello world")
+if amogus:
+    print("Jantz is impustor")
