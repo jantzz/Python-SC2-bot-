4fd8443c12792d5f1f35ff17c628a04337f1ace6
Dylan818 <limdylan818@gmail.com>
2022-05-29 16:59:32 +0800

    Fixed error cos jantz donkey

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-#github page:https://github.com/Dentosal/python-sc2
+# github page:https://github.com/Dentosal/python-sc2
 # documentation: https://github.com/Dentosal/python-sc2/wiki
-import sc2  as sc
+import sc2 as sc
+
 
 class Bot(sc.BotAI):
-    
+    pass
